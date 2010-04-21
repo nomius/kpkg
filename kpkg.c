baa1b760c3c2806347924070aec65210e3bca181
nomius <dcortarello@gmail.com>
2010-04-21 02:53:45 +0000

    Fixed typos. Documented the code. Fixed some memory leaks

--- kpkg.c
+++ kpkg.c
@@ -50,36 +50,43 @@ int RemovePkg(char *name, int silent)
 	int ret = 0;
 	PkgData Data;
 
+	/* Let's open the database */
 	if (sqlite3_open(dbname, &Database)) {
 		fprintf(stderr, "Failed to open database %s (%s)\n", dbname, sqlite3_errmsg(Database));
+		free(pathb);
 		return -1;
 	}
 
+	/* Switch to HOME_ROOT */
 	if (chdir(HOME_ROOT)) {
 		fprintf(stderr, "Can't go to %s (%s)\n", HOME_ROOT, strerror(errno));
 		free(pathb);
 		return -1;
 	}
 
+	/* Check if it is already installed */
 	memset(&Data, '\0', sizeof(Data));
 	strncpy(Data.name, name, PKG_NAME);
-
 	if (!ExistsPkg(&Data)) {
-		if (silent)
-			fprintf(stderr, "Package %s does not exists\n", name);
+		if (!silent)
+			fprintf(stderr, "Package %s does not exist\n", name);
 		chdir(pathb);
 		free(pathb);
 		return 1;
 	}
 
+	/* Remove all its files */
 	if (RemovePkgFiles(name) == -1) {
 		chdir(pathb);
 		free(pathb);
 		return -1;
 	}
 
+	/* Removing this package from the database */
 	ret = RemovePkgData(name);
 	sqlite3_close(Database);
+
+	/* Go back and clean up this mess */
 	chdir(pathb);
 	free(pathb);
 	fprintf(stdout, "Package %s removed\n", name);
@@ -100,60 +107,79 @@ int InstallPkg(char *package)
 	int fd = 0;
 	PkgData Data;
 
+	/* Save locations and initialize the package structure */
 	strncpy(PackageOrig, package, PATH_MAX);
 	init_path = getcwd(malloc(PATH_MAX), PATH_MAX);
 	memset(&Data, '\0', sizeof(PkgData));
 
+	/* Does it fisically exist? */
 	if ((fd = open(package, O_RDONLY)) < 0) {
 		/* Ok, the package doesn't exists, let's go for a mirror */
-		if (DownloadPkg(package, output) == -1)
+		if (DownloadPkg(package, output) == -1) {
+			free(init_path);
 			return -1;
+		}
 		package = output;
 	}
 	else 
 		close(fd);
+
+	/* Get the full pathname instead of its relative name */
 	ptr_name = (char *)basename(package);
 	pkgfullpath = (char *)dirname(package);
-
 	chdir(pkgfullpath);
 	pkgfullpath = getcwd(malloc(PATH_MAX), PATH_MAX);
 	snprintf(pkgfullpathname, PATH_MAX, "%s/%s", pkgfullpath, ptr_name);
 	free(pkgfullpath);
 
+	/* Switch to HOME_ROOT */
 	if (chdir(HOME_ROOT)) {
 		fprintf(stderr, "Can't go to %s (%s)\n", HOME_ROOT, strerror(errno));
 		free(init_path);
 		return -1;
 	}
 
+	/* Fill the package structure according to the package name */
 	if (FillPkgDataFromPackage(&Data, ptr_name)) {
 		fprintf(stdout, "Wrong package name format. It should be \"name-version-arch-build.ext\"\n");
 		chdir(init_path);
 		return -1;
 	}
 
-    if (sqlite3_open(dbname, &Database)) {
-        fprintf(stderr, "Failed to open database %s (%s)\n", dbname, sqlite3_errmsg(Database));
+	/* Open the database */
+	if (sqlite3_open(dbname, &Database)) {
+		fprintf(stderr, "Failed to open database %s (%s)\n", dbname, sqlite3_errmsg(Database));
 		chdir(init_path);
+		free(init_path);
         return -1;
-    }
+	}
 
+	/* Check if it is already installed */
 	if (ExistsPkg(Data.name)) {
 		fprintf(stderr, "Package %s already installed\n", PackageOrig);
 		chdir(init_path);
+		free(init_path);
 		return 1;
 	}
 
+	/* Get the package hash so we can register it in the database */
 	if (GiveMeHash(pkgfullpathname, Data.crc) == -1) {
 		chdir(init_path);
+		free(init_path);
 		return -1;
 	}
 
+	/*Extract the package itself */
 	if (ExtractPackage(pkgfullpathname, &Data) == -1) {
 		chdir(init_path);
+		free(init_path);
 		return -1;
 	}
+
+	/* Register the package in the database */
 	InsertPkgDB(&Data);
+
+	/* Clean everything up */
 	sqlite3_close(Database);
 	chdir(init_path);
 	fprintf(stdout, "Package %s installed\n", PackageOrig);
@@ -173,6 +199,7 @@ int DownloadPkg(char *name, char *out)
 	char *input = NULL;
 	char *MIRROR = NULL;
 
+	/* Initialize everything up */
 	Links.index = 0;
 	Links.links = NULL;
 	Links.comments = NULL;
@@ -184,11 +211,10 @@ int DownloadPkg(char *name, char *out)
 		if (SearchLinkForPackage(name, &Links, MIRROR))
 			return 1;
 	}
-	else {
-		if (SearchLinkForPackage(name, &Links, NULL))
-			return 1;
-	}
+	else if (SearchLinkForPackage(name, &Links, NULL))
+		return 1;
 
+	/* This is really ugly, and unfortunally we have to handle it */
 	if (Links.index > 1) {
 		/* Print out all the links with their associated numbers */
 		while(i < Links.index) {
@@ -211,7 +237,7 @@ int DownloadPkg(char *name, char *out)
 				free(input);
 				return 1;
 			}
-			/* input the "c" letter and its number to get the link description */
+			/* input the "c" letter and its number to get the link comments */
 			else if (*input == 'c') {
 				i = atoi(input+1);
 				if (i < Links.index && i >= 0)
@@ -243,7 +269,7 @@ int DownloadPkg(char *name, char *out)
 	if (Links.links[i][j] == '/') j++;
 	sprintf(filename, "%s/%s", PACKAGES_DIRECTORY, &(Links.links[i][j]));
 
-	/* If the output already exists and the CRC is ok, then great, we already have it, so there's no need to download it again */
+	/* If the output already exist and the CRC is ok, then great, we already have it, so there's no need to download it again */
 	if ((fd = open(filename, O_RDONLY)) >= 0) {
 		close(fd);
 		if (GiveMeHash(filename, pkgcrc) == -1) {
@@ -253,6 +279,7 @@ int DownloadPkg(char *name, char *out)
 		if (strcmp(pkgcrc, Links.crcs[i]))
 			fprintf(stderr, "CRC error, Downloading the package again...\n");
 		else {
+			/* Great, it's already installed, so we leave */
 			if (out)
 				strcpy(out, filename);
 			freeLinks(&Links);
@@ -304,6 +331,7 @@ int UpdateMirrorDB(char *db)
 
 	/* If only one database was issued */
 	if (db) {
+
 		/* Get the path */
 		snprintf(dbpath, PATH_MAX, "%s/%s.db", MIRRORS_DIRECTORY, db);
 		if ((fd = open(dbpath, O_RDONLY)) < 0) {
@@ -311,15 +339,19 @@ int UpdateMirrorDB(char *db)
 			return 1;
 		}
 		close(fd);
+
+		/* Get its data */
 		memset(dbLink, '\0', sizeof(dbLink));
 		memset(dbDesc, '\0', sizeof(dbDesc));
-		/* Get its data */
 		if (GetDataFromMirrorDatabase(dbpath, "LINK", dbLink) == -1)
 			return -1;
 		if (GetDataFromMirrorDatabase(dbpath, "DESC", dbDesc) == -1)
 			return -1;
+
+		/* Show comments for this database if any */
 		if (dbDesc[0] != '\0')
 			fprintf(stdout, "Comments for [%s]:\n%s\n", db, dbDesc);
+
 		/* Get the database! */
 		if (dbLink[0] != '\0') {
 			if (Download(dbLink, dbpath) == -1)
@@ -336,23 +368,32 @@ int UpdateMirrorDB(char *db)
 	}
 
 	while ((dit = readdir(dip)) != NULL) {
+		/* Skip directories . and  .. */
 		if (!strcmp(dit->d_name, ".") || !strcmp(dit->d_name, ".."))
 			continue;
+
+		/* Get the full path name for that database */
 		snprintf(dbpath, PATH_MAX, "%s/%s", MIRRORS_DIRECTORY, dit->d_name);
+
+		/* Get its data */
 		memset(dbLink, '\0', sizeof(dbLink));
 		memset(dbDesc, '\0', sizeof(dbDesc));
-		/* Get its data */
 		if (GetDataFromMirrorDatabase(dbpath, "LINK", dbLink) == -1)
 			return -1;
 		if (GetDataFromMirrorDatabase(dbpath, "DESC", dbDesc) == -1)
 			return -1;
+
+		/* Show comments for this database if any */
 		if (dbDesc[0] != '\0') {
 			dbNiceName = strdup(dit->d_name);
+
+			/* Remove the extension to display it nicely with its comment */
 			for (i = strlen(dbNiceName)-1;dbNiceName[i]!='.' && i>=0;i--) ;
 			if (i > 0) dbNiceName[i] = '\0';
 			fprintf(stdout, "Comments for [%s]:\n%s\n", dbNiceName, dbDesc);
 			free(dbNiceName);
 		}
+
 		/* Get the database! */
 		if (Download(dbLink, dbpath) == -1)
 			return -1;
@@ -380,16 +421,19 @@ int UpgradePkg(char *package)
 	if (package) {
 		/* Upgrade a single package */
 		if ((i = open(package, O_RDONLY)) >= 0) {
-			/* Ok, the package exists */
+			/* Ok, the package itself was given */
 			close(i);
+			/* Get the package data from its name */
 			if (FillPkgDataFromPackage(&Data, basename(package))) {
 				fprintf(stdout, "Wrong package name format. It should be \"name-version-arch-build.ext\"\n");
 				return -1;
 			}
+			/* Remove the package */
 			if (RemovePkg(Data.name, 1) == -1) {
 				fprintf(stderr, "Failed to remove the old version of %s\n", Data.name);
 				return -1;
 			}
+			/* Install the package */
 			if (InstallPkg(package) == -1) {
 				fprintf(stderr, "Could not install the new version of %s\n", Data.name);
 				return -1;
@@ -397,20 +441,27 @@ int UpgradePkg(char *package)
 			return 0;
 		}
 		else {
-			/* Ok the package wasn't phisically given */
+			/* Ok the package wasn't actually given, but a name */
 			strncpy(Data.name, package, PKG_NAME);
-		    if (!ExistsPkg(&Data)) {
+			if (!ExistsPkg(&Data)) {
 				fprintf(stdout, "Package %s isn't installed (perhaps you meant install?)\n", package);
 				return 1;
 			}
+			/* Check if there's a new version available on the mirror */
 			if ((ret = NewVersionAvailable(&Data, MIRROR))) {
 				setenv("MIRROR", MIRROR, 1);
+
+				/* Great, found a new version, download it */
 				if (DownloadPkg(Data.name, NULL) == -1)
 					return -1;
+
+				/* Remove the old one */
 				if ((ret = RemovePkg(Data.name, 1)) == -1) {
 					fprintf(stderr, "Failed to remove the old version of %s\n", Data.name);
 					return -1;
 				}
+
+				/* Install it */
 				if (InstallPkg(Data.name) == -1) {
 					fprintf(stderr, "Could not install the new version of %s\n", Data.name);
 				}
@@ -427,6 +478,7 @@ int UpgradePkg(char *package)
 			strncpy(Data.name, Packages.packages[i], PKG_NAME);
 			strncpy(Data.version, Packages.versions[i], PKG_VERSION);
 			strncpy(Data.build, Packages.builds[i], PKG_BUILD);
+
 			/* Check for new versions in mirrors */
 			if ((ret = NewVersionAvailable(&Data, MIRROR))) {
 				setenv("MIRROR", MIRROR, 1);
@@ -466,7 +518,7 @@ void say_help(int status)
 " provides         search for files inside of installed packages\n"
 " download         download a package from a mirror"
 "\n"
-"Kpkg 4.0a by David B. Cortarello (Nomius) <dcortarello@gmail.com>\n");
+"Kpkg 4.0a by David B. Cortarello (Nomius) <dcortarello@gmail.com>\n\n");
 	exit(status);
 }
 
